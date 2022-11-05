52ee6caca4a1fb8f0d5daf86b088521d4dff3560
Natália Belo <nataliabelo.nb@gmail.com>
2022-11-05 08:38:31 -0300

    Date Picker

--- cypress/e2e/exerc05/webdriveruniversity.cy.js
+++ cypress/e2e/exerc05/webdriveruniversity.cy.js
@@ -1,3 +1,5 @@
+import "cypress-iframe";
+
 describe("Web Driver UNiversity", () => {
   beforeEach("Acesso Home Page", () => {
     cy.visit("http://webdriveruniversity.com/");
@@ -14,20 +16,32 @@ describe("Web Driver UNiversity", () => {
     cy.get(".modal-footer > .btn", { timeout: 15000 }).click();
   });
 
-  it.only("Iframe", () => {
+  it("Iframe", () => {
     cy.get('a[href="IFrame/index.html"]')
       .should("be.visible")
       .invoke("removeAttr", "target")
       .click();
+    cy.frameLoaded("iframe[role='presentation']");
+    cy.iframe().xpath("//span[text()='Maps']").click();
     cy.get('a[href="../Contact-Us/contactus.html"]', { timeout: 15000 })
       .should("be.visible")
       .invoke("removeAttr", "target")
       .click();
   });
 
-  it("Date Picker", () => {
+  it.only("Date Picker", () => {
+    const hoje = new Date();
+    const dia = hoje.getDate().toString().padStart(2, "0");
+    const mes = String(hoje.getMonth() + 1).padStart(2, "0");
+    const ano = hoje.getFullYear();
+    const dataAtual = `${mes} / ${dia - 1} / ${ano}`;
+
     cy.get('a[href="Datepicker/index.html"]')
       .should("be.visible")
-      .invoke("removeAttr", "target");
+      .invoke("removeAttr", "target")
+      .click();
+
+    cy.get("#datepicker input").clear({ force: true });
+    cy.get('input[type="text"]').type(dataAtual, { force: true });
   });
 });
