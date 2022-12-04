4e0d3a714880b2910fb37ef85df7023beffd17c7
Natália Belo <nataliabelo.nb@gmail.com>
2022-12-04 11:23:28 -0300

    preenchendo formulário contactUs

--- cypress/e2e/exerc05/webdriveruniversity.cy.js
+++ cypress/e2e/exerc05/webdriveruniversity.cy.js
@@ -1,5 +1,10 @@
 import "cypress-iframe";
 
+import elements, {
+  ELEMENTSUniversity,
+  contactUs,
+} from "../../support/constants/elements";
+
 describe("Web Driver UNiversity", () => {
   beforeEach("Acesso Home Page", () => {
     cy.visit("http://webdriveruniversity.com/");
@@ -12,21 +17,31 @@ describe("Web Driver UNiversity", () => {
       .click();
 
     cy.get("#button1 > p", { timeout: 15000 }).should("be.visible").click();
-
+    cy.wait(3000);
     cy.get(".modal-footer > .btn", { timeout: 15000 }).click();
   });
 
   it.only("Iframe", () => {
-    cy.get('a[href="IFrame/index.html"]')
-      .should("be.visible")
-      .invoke("removeAttr", "target")
-      .click();
-    cy.frameLoaded("#frame");
-    cy.iframe("#frame")
-      .find('a[href="../Contact-Us/contactus.html"]')
-      .should("be.visible")
-      .invoke("removeAttr", "target")
-      .click();
+    // cy.get('a[href="IFrame/index.html"]')
+    //   .should("be.visible")
+    //   .invoke("removeAttr", "target")
+    //   .click();
+    // cy.frameLoaded("#frame");
+    // cy.iframe("#frame")
+    //   .find('a[href="../Contact-Us/contactus.html"]')
+    //   .should("be.visible")
+    //   .invoke("removeAttr", "target")
+    //   .click();
+    cy.get("#contact-us").invoke("removeAttr", "target").click();
+    cy.get(ELEMENTSUniversity.firstName).type(contactUs.name);
+    cy.get(ELEMENTSUniversity.lastName).type(contactUs.lastName);
+    cy.get(ELEMENTSUniversity.emailAddress).type(contactUs.emailAddress);
+    cy.get(ELEMENTSUniversity.comments).type("testando");
+    cy.get(ELEMENTSUniversity.submit).click();
+    cy.get("#contact_reply > h1").should(
+      "have.text",
+      "Thank You for your Message!"
+    );
   });
 
   it("Date Picker", () => {
